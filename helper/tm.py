814f17c2d9263dda1b3e1937d3800dc45718a1ad
plsy1 <chisan1230@protonmail.com>
2024-12-01 20:23:05 +0800

    .

--- helper/tm.py
+++ helper/tm.py
@@ -12,14 +12,14 @@ def move_books():
         if filename == 'cover.jpg':
             continue
         # Define the regular expression pattern to match the date in the format: dd MMM yyyy
-        match = re.search(r"\[([A-Za-z]{3})\s(\d{2})\]", filename)
+        
+        match = re.search(r"\[([A-Za-z]+)\s(\d{1,2})(?:st|nd|rd|th)?,\s(\d{4})\]", filename)
 
         if match:
-            month_str = match.group(1)
-            day = match.group(2)
-            year = str(datetime.now().year)
-            # Convert the month abbreviation to a number
-            month = datetime.strptime(month_str, "%b").month
+            month_str = match.group(1)  
+            month = datetime.strptime(month_str, "%B").month
+            day = match.group(2)        
+            year = match.group(3)      
 
             # Create a date string in the format yyyymmdd
             date_str = f"{year}{month:02d}{day}"
