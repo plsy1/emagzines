ae914948346c241ead857e09cec505c09bb5a522
y1 <39407459+plsy1@users.noreply.github.com>
2025-04-29 22:07:25 +0800

    Update te.py

--- helper/te.py
+++ helper/te.py
@@ -11,33 +11,35 @@ def move_books():
         if filename == "cover.jpg":
             continue
 
-        match = re.search(r"The\sEconomist\s\[(\w+)\s(\d{1,2}),\s(\d{4})\]", filename)
+        match = re.search(r"\[([A-Za-z]+)\s(\d{1,2})(?:st|nd|rd|th)?,\s(\d{4})\]", filename)
 
         if match:
-            month_str = match.group(1)
-            day = int(match.group(2))
-            year = match.group(3)
+            month_str = match.group(1)  
+            month = datetime.strptime(month_str, "%B").month
+            day = int(match.group(2))        
+            year = match.group(3)      
 
-            month = datetime.strptime(month_str, "%b").month
-
+            # Create a date string in the format yyyymmdd
             date_str = f"{year}{month:02d}{day:02d}"
+        else:
+            date_str = datetime.now().strftime('%Y%m%d')
 
-            extension = os.path.splitext(filename)[1]
+        extension = os.path.splitext(filename)[1]
 
-            new_filename = f"{date_str} - The Economist{extension}"
+        new_filename = f"{date_str} - The Economist{extension}"
 
-            old_file_path = os.path.join(books_dir, filename)
+        old_file_path = os.path.join(books_dir, filename)
 
-            date_dir = os.path.join(books_dir, date_str)
-            if not os.path.exists(date_dir):
-                os.mkdir(date_dir)
+        date_dir = os.path.join(books_dir, date_str)
+        if not os.path.exists(date_dir):
+            os.mkdir(date_dir)
 
-            new_file_path = os.path.join(date_dir, new_filename)
+        new_file_path = os.path.join(date_dir, new_filename)
 
-            os.rename(old_file_path, new_file_path)
-            print(f"Book Renamed: {filename} -> {new_filename}")
+        os.rename(old_file_path, new_file_path)
+        print(f"Book Renamed: {filename} -> {new_filename}")
 
-    return f"{books_dir}/{date_str}", date_str
+        return f"{books_dir}/{date_str}", date_str
 
 
 def move_cover(newDir):
